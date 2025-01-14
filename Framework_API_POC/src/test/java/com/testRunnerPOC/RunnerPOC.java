f9a381ea80ae4fc53e8436dafef4803c2df45b0b
Franklin Sv <frankysv04@gmail.com>
2025-01-14 10:54:14 +0530

    Update RunnerPOC.java

--- Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
+++ Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
@@ -7,8 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src//test//java//com//featurePOC//HotelBooking.feature", glue = {
-		"com.stepDefPOC" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"json:target/cucumber.json","html:target/cucumber-reports.html" })
+		"com.stepDefPOC" }, plugin = {"html:target/cucumber-reports.html", "json:target/report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"json:target/cucumber.json"})
 public class RunnerPOC {
 
 	public static void main(String[] args) {
