2d89a2205b5af7fbd28d7a5108cd8761e29e94d9
frankysv0 <frankysv04@gmail.com>
2025-01-14 10:14:11 +0530

    Runner class

--- Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
+++ Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C://Users//fselvakumar//New Eclipse 2024//Framework_API_POC//src//test//java//com//featurePOC//HotelBooking.feature", glue = {
+@CucumberOptions(features = "src//test//java//com//featurePOC//HotelBooking.feature", glue = {
 		"com.stepDefPOC" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"json:target/cucumber.json","html:target/cucumber-reports.html" })
 public class RunnerPOC {
